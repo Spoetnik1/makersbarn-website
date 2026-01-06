45fba7845b7a41806d2e24760f3c09dceeb13639
Benny Vaksendiser <benny.vaks@gmail.com>
2026-01-06 11:02:48 +0100

    Set Google Maps embed to satellite view by default

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -3,7 +3,7 @@ export const CONTACT_PHONE_NUMBER = '+31649778965'
 export const CONTACT_URLS = {
   WHATSAPP: `https://wa.me/${CONTACT_PHONE_NUMBER.replace('+', '')}`,
   PHONE: `tel:${CONTACT_PHONE_NUMBER}`,
-  MAP_EMBED: 'https://www.google.com/maps?q=Duisterendijk+2+8131+RA+Wijhe&output=embed',
+  MAP_EMBED: 'https://www.google.com/maps?q=Duisterendijk+2+8131+RA+Wijhe&t=k&output=embed',
 } as const
 
 export const ANCHOR_IDS = {
